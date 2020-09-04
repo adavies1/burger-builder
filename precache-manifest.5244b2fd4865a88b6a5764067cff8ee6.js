f08c26eda5df6955c089d37f0654267eba5eddf4
Alex Davies <alexdavieswebdev@gmail.com>
2020-09-04 10:49:07 +0100

    Updates

---
+++ precache-manifest.5244b2fd4865a88b6a5764067cff8ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17578d2c7dcda65d80d00808be7893e",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "a1782c3a900bad9c288d",
+    "url": "/burger-builder/static/css/main.f75b2db9.chunk.css"
+  },
+  {
+    "revision": "609ad46c266c0ab9eba6",
+    "url": "/burger-builder/static/js/2.451256ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder/static/js/2.451256ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1782c3a900bad9c288d",
+    "url": "/burger-builder/static/js/main.514b529b.chunk.js"
+  },
+  {
+    "revision": "7bb0118002718fd73a9a",
+    "url": "/burger-builder/static/js/runtime-main.5fc61628.js"
+  }
+]);
